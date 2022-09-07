d5343253fb0ae884be023a5212cc42b7429c03ad
Oleg Pashuk <45397736+olegfour3@users.noreply.github.com>
2022-09-07 09:27:05 +0300

    добавил shell скрипт для запуска бота на линукс системах

---
+++ start_bot.sh
@@ -0,0 +1,8 @@
+export TOKEN=5407094725:123123123123moPJ50G5123123123123lO5b4 # your bot token
+export ADMIN_ID=111111111 # your telegram id
+
+source /home/tb/vocalMasterTelegramBot/venvBOT/bin/activate
+cd /home/tb/vocalMasterTelegramBot/
+
+#!/bin/bash
+python3.10 main.py
